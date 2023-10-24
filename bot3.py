5f503fd8ddea83d32753b46f51ff8e1ada53933a
MehMasha <mehmasha69@gmail.com>
2023-10-24 22:45:37 +0300

    initial

---
+++ bot3.py
@@ -0,0 +1,71 @@
+import json
+from websockets.sync.client import connect
+import requests
+
+def move(text, username):
+    if text in ['w', 'W', 'forward', 'в', 'вперед']:
+        requests.get(f'http://192.168.1.90/Forward?q={username}')
+    if text in ['s', 'S', 'backward', 'н', 'назад']:
+        requests.get(f'http://192.168.1.90/Backward?q={username}')
+    if text in ['a', 'A', 'left', 'л', 'лево']:
+        requests.get(f'http://192.168.1.90/Left?q={username}')
+    if text in ['d', 'D', 'right', 'п', 'право']:
+        requests.get(f'http://192.168.1.90/Right?q={username}')
+
+
+def hello():
+    with connect("ws://localhost:8383/Chat") as websocket:
+        while True:
+            message = websocket.recv()
+            js = json.loads(message)
+            text = js.get('text_text')
+            username = js.get('user')
+            user_id = js.get('user_id')
+            site = js.get('site_cut')
+            try:
+                # if username in ['ve5pi']:
+
+
+                    text = text.strip().lower()
+                    print(text)
+                    username = js.get('user')
+                    if len(text) == 2 and text[0] in ['w', 's', 'в', 'н']:
+                        text, num = list(text)
+                        num = int(num)
+                        if 1 <= num <= 5:
+                            for i in range(num):
+                                move(text, username)
+                    else:
+                        move(text, username)
+
+                    # if text == 'buzz':
+                    #     requests.get(f'http://192.168.1.90/Buzz')
+
+                    # if text == 'off':
+                    #     requests.get(f'http://192.168.1.90/BlinkRightOff')
+                    #     requests.get(f'http://192.168.1.90/BlinkLeftOff')
+                    # if text == 'on':
+                    #     requests.get(f'http://192.168.1.90/BlinkRightOn')
+                    #     requests.get(f'http://192.168.1.90/BlinkLeftOn')
+                    if text == 'e':
+                        requests.get(f'http://192.168.1.90/Turn?q={0}')
+                    if text == 'q':
+                        requests.get(f'http://192.168.1.90/Turn?q={180}')
+                    if text == 'rf':
+                        requests.get(f'http://192.168.1.90/Turn?q={90}')
+                    
+                    # if text == 'f':
+                    #     requests.get(f'http://192.168.1.90/Buzz')
+                # if len(text) >= 1:
+                #     command = text[0]
+                #     if command == '!move':
+                #         if len(text) == 3:
+                #             start = text[1]
+                #             finish = text[2]
+                #             q.put_nowait([start, finish, username, site])
+                #             print(f'{username} с {site} походил {start} {finish}')
+            except:
+                pass
+
+
+hello()
